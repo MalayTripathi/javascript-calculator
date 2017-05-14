cf1ba4c44d77ecd26fd3948ff2baf40efdae3ec5
Malay Tripathi <malay_audioslave@hotmail.com>
2017-05-14 19:07:21 +0530

    Structured

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var view = require('./routes/view');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', view);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
